015793c26e0756b109968b71d928056488b49720
Swiftian <88419676+ThinkSwift@users.noreply.github.com>
2025-09-04 12:42:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,12 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "PlainStoreKit",
-    platforms: [.iOS(.v17)],
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10)
+        // Catalyst도 쓴다면: .macCatalyst(.v17) // Xcode 15.4+ 문법
+    ],
     products: [
-        .library(name: "PlainStoreKit", targets: ["PlainStoreKit"])
+        .library(name: "PlainStoreKit", targets: ["PlainStoreKit"]),
     ],
     targets: [
         .target(name: "PlainStoreKit"),
-        .testTarget(name: "PlainStoreKitTests", dependencies: ["PlainStoreKit"], path: "Tests")
+        .testTarget(name: "PlainStoreKitTests", dependencies: ["PlainStoreKit"]),
     ]
 )
