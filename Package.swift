91abd1420da3a130003774b402bce53cfe4d2cb7
Swiftian <88419676+ThinkSwift@users.noreply.github.com>
2025-09-04 13:17:24 +0900

    Update Package.swift to remove tvOS and watchOS
    Removed support for tvOS and watchOS platforms in the package configuration.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "PlainStoreKit",
     platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-        .tvOS(.v17),
-        .watchOS(.v10)
+        .iOS(.v17),   // SwiftData
+        .macOS(.v14)
     ],
     products: [
-        .library(name: "PlainStoreKit", targets: ["PlainStoreKit"]),
+        .library(name: "PlainStoreKit", targets: ["PlainStoreKit"])
     ],
     targets: [
-        .target(name: "PlainStoreKit"),
-        .testTarget(name: "PlainStoreKitTests", dependencies: ["PlainStoreKit"]),
+        .target(
+            name: "PlainStoreKit",
+            path: "Sources/PlainStoreKit"
+        )
     ]
 )
